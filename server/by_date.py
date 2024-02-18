9e3626a2d32fdbf79910b95847b1e7f62490aed3
Paul Tuck <paul.tuck@oxon.org>
2024-02-18 21:46:35 +0700

    $user_commit_msg"

--- server/by_date.py
+++ server/by_date.py
@@ -1,24 +1,36 @@
+from dotenv import load_dotenv
 import http.client
 import datetime
+import os
 
-# Get today's date in the correct format
-today = datetime.date.today().strftime("%Y-%m-%d")
-
-timezone = "Asia/Bangkok"
-
-conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+# Load environmental variables
+load_dotenv()
 
 headers = {
     "x-rapidapi-host": "v3.football.api-sports.io",
-    "x-rapidapi-key": "XxXxXxXxXxXxXxXxXxXxXxXx",  # Make sure to replace this with your actual API key
+    "x-rapidapi-key": os.getenv("X_RAPIDAPI_KEY"),
 }
+conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+
+# Get today's date in the correct format
+timezone = "Asia/Bangkok"
+today = datetime.date.today().strftime("%Y-%m-%d")
+league = 39  # English Premier League
 
 # Append your queries here
 conn.request(
-    "GET", f"/fixtures?timezone={timezone}&league=39&date={today}", headers=headers
+    "GET",
+    f"/fixtures?timezone={timezone}&league={league}&date={today}",
+    headers=headers,
 )
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+print(data.decode("utf-8"))  # Write data to the terminal
+
+# Write to a file
+with open("fixtures.json", "w") as f:
+    f.write(data.decode("utf-8"))
+
+print("Data written to fixtures.json")
