e28173c4130a2e057c7be8acd32563c654ac4817
Paul Tuck <paul.tuck@oxon.org>
2024-02-20 17:36:50 +0700

    $user_commit_msg"

--- server/core/models/competition.py
+++ server/core/models/competition.py
@@ -4,7 +4,10 @@ from django.db import models
 class Competition(models.Model):
     competition_id = models.IntegerField()
     name = models.CharField(max_length=128)
+    season = models.CharField(max_length=16)
+    round = models.CharField(max_length=32)
     description = models.TextField()
-    logo = models.ImageField(upload_to="competitions/")
+    logo = models.ImageField(upload_to="media/competitions/")
+    flag = models.ImageField(upload_to="media/flags/")
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
